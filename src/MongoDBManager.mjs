d3c2f7e0438c44e4bc51339727cd1d62ee5d2359
daosgava <daosgava.garcia@gmail.com>
2024-05-14 16:17:47 +1000

    add mongo manager

---
+++ src/MongoDBManager.mjs
@@ -0,0 +1,55 @@
+import { MongoClient } from "mongodb";
+
+class MongoDBManager {
+	constructor(url, dbName, collectionName) {
+		this.url = url;
+		this.dbName = dbName;
+		this.collectionName = collectionName;
+		this.client = new MongoClient(this.url);
+	}
+
+	connect = async () => {
+		if (!this.db) {
+			this.client = await this.client.connect();
+			this.db = this.client.db(this.dbName);
+			this.collection = this.db.collection(this.collectionName);
+		}
+	};
+
+	insertDocument = async (req, res) => {
+		const data = req.body;
+		if (!data) {
+			res.status(400).send("No data provided");
+		}
+
+		try {
+			await this.connect();
+			const result = await this.collection.insertOne(data);
+			console.log(`Inserted document with id: ${result.insertedId}`);
+			res.send(result);
+		} catch (e) {
+			console.error(e);
+			res.status(500).send("Internal server error");
+		}
+	};
+
+	getDocuments = async (_, res) => {
+		try {
+			await this.connect();
+			const documents = await this.collection.find({}).toArray();
+			console.log(`Found documents: ${documents}`);
+			res.send(documents);
+		} catch (e) {
+			console.error(e);
+			res.status(500).send("Internal server error");
+		}
+	};
+
+	close = async () => {
+		if (this.client) {
+			await this.client.close();
+		}
+	};
+}
+
+export default MongoDBManager;
--- src/index.mjs
+++ src/index.mjs
@@ -1,19 +1,15 @@
 import express from "express";
 import Operations from "./operations.mjs";
 import { port, paths } from "./constants.mjs";
-import { MongoClient } from "mongodb";
+import MongoDBManager from "./MongoDBManager.mjs";
 import bodyParser from "body-parser";
-
-const url = "mongodb://admin:password@mongo-service:32000";
-const dbName = "testDatabase";
-const collectionName = "testCollection";
 
 const app = express();
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Instantiate OperationHandler
 const operations = new Operations();
@@ -23,45 +19,16 @@ for (const path of Object.values(paths)) {
 	app.get(path, operations.requestHandler);
 }
 
-app.get("/break-app", (_, res) => {
-	res.send("🔥: This is a broken route");
-	process.exit(1);
-});
+const dbUser = process.env.MONGO_INITDB_ROOT_USERNAME;
+const dbPassword = process.env.MONGO_INITDB_ROOT_PASSWORD;
 
-app.get("/get-entries", async (req, res) => {
-	try {
-        const client = await MongoClient.connect(url);
+const url = `mongodb://${dbUser}:${dbPassword}@mongo-service:32000`;
+const dbName = "testDatabase";
+const collectionName = "testCollection";
+const mongoDBManager = new MongoDBManager(url, dbName, collectionName);
 
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
-		const documents = await collection.find({}).toArray();
-
-        res.send(documents);
-	} catch (e) {
-		console.error(e);
-		res.status(500).send("Internal server error");
-	}
-});
-
-app.post("/add-entry", async (req, res) => {
-	const data = req.body;
-	if (!data) {
-		res.status(400).send("No data provided");
-	}
-
-	try {
-		const client = await MongoClient.connect(url);
-
-		const db = client.db(dbName);
-		const collection = db.collection(collectionName);
-		const result = await collection.insertOne(data);
-
-		res.send(result);
-	} catch (e) {
-		console.error(e);
-		res.status(500).send("Internal server error");
-	}
-});
+app.get("/documents", mongoDBManager.getDocuments);
+app.post("/document", mongoDBManager.insertDocument);
 
 app.listen(port, () => {
 	console.log(`🐲: This server runs at http://localhost:${port}`);
