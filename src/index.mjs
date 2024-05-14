fed9b5867a35b71aea544e51567cf4fa703fb4c0
daosgava <daosgava.garcia@gmail.com>
2024-05-14 15:18:52 +1000

    add mongo db connection to calculator api

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,20 @@
 import express from "express";
 import Operations from "./operations.mjs";
 import { port, paths } from "./constants.mjs";
+import { MongoClient } from "mongodb";
+import bodyParser from "body-parser";
+
+const url = "mongodb://admin:password@mongo-service:32000";
+const dbName = "testDatabase";
+const collectionName = "testCollection";
 
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 // Instantiate OperationHandler
 const operations = new Operations();
 
@@ -14,6 +26,40 @@ for (const path of Object.values(paths)) {
 app.get("/break-app", (_, res) => {
 	res.send("🔥: This is a broken route");
 	process.exit(1);
+});
+
+app.get("/get-entries", async (req, res) => {
+	try {
+        const client = await MongoClient.connect(url);
+
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+		const documents = await collection.find({}).toArray();
+
+        res.send(documents);
+	} catch (e) {
+		console.error(e);
+	}
+});
+
+app.post("/add-entry", async (req, res) => {
+	console.log({body: req.body});
+	const data = req.body;
+	if (!data) {
+		res.status(400).send("No data provided");
+	}
+
+	try {
+		const client = await MongoClient.connect(url);
+
+		const db = client.db(dbName);
+		const collection = db.collection(collectionName);
+		const result = await collection.insertOne(data);
+
+		res.send(result);
+	} catch (e) {
+		console.error(e);
+	}
 });
 
 app.listen(port, () => {
